6a607f077a6070f42a8ea39b86733517b85b8ac2
Benjamin Loglisci <loglisci@hotmail.fr>
2017-10-12 03:53:10 +0200

    continuation TME1

--- Code/Index.py
+++ Code/Index.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
 class Index(object):
-    
+
     def indexation(self, file):
         raise NotImplementedError()
-    
+
     def getTfsForDoc(self, doc):
         raise NotImplementedError()
-    
+
     def getTfsForStem(self, stem):
         raise NotImplementedError()
-    
+
     def getStrDoc(self, doc):
         raise NotImplementedError()
 
 class IndexOnFile(Index):
-    
+
     def __init__(self, name, parser, textRepresenter):
         self.name = name
         self.docs = {}
@@ -23,22 +23,23 @@ class IndexOnFile(Index):
         self.docFrom = {}
         self.parser = parser
         self.textRepresenter = textRepresenter
-    
-    def createFiles(path = "../Data"):
+
+    def createFiles(self, path):
+        #path = "../Data"
         open(path + "/" + self.name + "_index", "x").close()
         open(path + "/" + self.name + "_inverted", "x").close()
-    
-    def indexation(self, file):
-        self.parser.initFile(file)
+
+    def indexation(self, path):
+        self.parser.initFile(path)
         fi = open(path + "/" + self.name + "_index", "w+b")
         fs = open(path + "/" + self.name + "_inverted", "w+b")
-        curi = 0
-        curs = 0
+        cur = 0
         doc = self.parser.nextDocument()
         while(doc):
             stems = self.textRepresenter(doc.getText())
             fi.write((doc.getid() + " : " + str(stems)).encode())
+            self.docs{doc.getid()} = (cur,fi.tell())
+            cur = fi.tell()
             doc = self.parser.nextDocument()
-            
-    
-    
+
+
