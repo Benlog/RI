d8ced2beedda70ee0de8f663b39056a8fd9170c9
André Nasturas <andre.nasturas@delfosia.net>
2018-01-29 02:25:09 +0100

    diveristy début

---
+++ Code/DiversityModel.py
@@ -0,0 +1,45 @@
+import numpy as np
+from IRmodel import IRmodel
+
+
+class ClusterDiversityModel(IRmodel):
+    def __init__(self, model, clustering, index, weighter):  # arg ord ?
+        super().__init__(index, weighter)
+        self.model = model            # Baseline
+        self.clustering = clustering  # Algo de clustering
+
+    def getScores(self, query, n=100):
+
+        result = self.model.getRanking(query)[:n]
+        voc = {s for (r, _) in result for s in self.weighter.getDocWeightsForDoc(r)}
+        dic = {r: [r[s] if s in r else 0 for s in self.index.stems]
+               for (r, _) in result}
+
+        clr = self.clustering(dic)
+
+        r = []
+
+        j = 0
+        c = len(clr)
+
+        while c:
+            c = len(clr)
+            for i in clr:
+                if len(i) > j:
+                    r.append(i[j])
+                else:
+                    c -= 1
+            j += 1
+
+
+
+class GreedyDiversityModel(ClusterDiversityModel):
+
+    def getScores(self, query, n=100):
+
+        result = self.model.getRanking(query)[:n]
+        voc = {s for (r, _) in result for s in self.weighter.getDocWeightsForDoc(r)}
+        dic = {r: [r[s] if s in r else 0 for s in self.index.stems]
+               for (r, s) in result}
+
+        self.clustering
